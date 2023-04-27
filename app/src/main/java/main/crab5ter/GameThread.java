17af222b60edb892e5a467678ee325802b07a703
Abdulsalam Muhialdeen <slome.lord123@gmail.com>
2023-04-28 00:58:09 +0200

    Ball example

---
+++ app/src/main/java/main/crab5ter/GameThread.java
@@ -0,0 +1,39 @@
+package main.crab5ter;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+    private final SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private boolean running;
+
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void run() {
+        Canvas canvas;
+        while (running) {
+            canvas = null;
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    gameView.update();
+                    gameView.draw(canvas);
+                }
+            } finally {
+                if (canvas != null) {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/main/crab5ter/GameView.java
@@ -0,0 +1,130 @@
+package main.crab5ter;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private float ballX, ballY; // current position of the ball
+    private float ballSize; // size of the ball
+    private Paint ballPaint;
+    private boolean gameOver;
+
+    private GameThread thread;
+
+    private float ballSpeedX, ballSpeedY; // current speed of the ball
+
+    public GameView(Context context) {
+        super(context);
+        init();
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        getHolder().addCallback(this);
+        ballPaint = new Paint();
+        ballPaint.setColor(Color.BLUE);
+        ballSize = 50;
+        gameOver = false;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // initialize game state
+        ballX = getWidth() / 2;
+        ballY = getHeight() / 2;
+        ballSpeedX = 0;
+        ballSpeedY = 0;
+
+        // start the game thread
+        thread = new GameThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop the game thread
+        boolean retry = true;
+        thread.setRunning(false);
+        while (retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    public void updatePosition(double ax, double ay) {
+        // update ball speed based on accelerometer sensor values
+        ballSpeedX += -ax/5 * 0.95f;
+        ballSpeedY += ay/5  * 0.95f;
+    }
+
+    public void update() {
+        // check for collisions with screen edges
+        if (ballX - ballSize < 0 || ballX + ballSize > getWidth()) {
+            ballSpeedX = -ballSpeedX * 0.3f; // reverse speed and apply friction
+            if (ballX - ballSize < 0) {
+                ballX = ballSize;
+            } else {
+                ballX = getWidth() - ballSize;
+            }
+        }
+        if (ballY - ballSize < 0 || ballY + ballSize > getHeight()) {
+            ballSpeedY = -ballSpeedY * 0.3f; // reverse speed and apply friction
+            if (ballY - ballSize < 0) {
+                ballY = ballSize;
+            } else {
+                ballY = getHeight() - ballSize;
+            }
+        }
+
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        // check if ball is out of bounds
+        //if (ballX < ballSize || ballX > getWidth() - ballSize || ballY < ballSize || ballY > getHeight() - ballSize) {
+        //    gameOver = true;
+        //}
+    }
+
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawCircle(ballX, ballY, ballSize, ballPaint);
+        if (gameOver) {
+            Paint textPaint = new Paint();
+            textPaint.setColor(Color.RED);
+            textPaint.setTextSize(100);
+            String gameOverText = "GAME OVER";
+            canvas.drawText(gameOverText, getWidth() / 2 - textPaint.measureText(gameOverText) / 2, getHeight() / 2, textPaint);
+        }
+    }
+
+    public void pause() {
+        // not used in this example
+    }
+
+    public void resume() {
+        // not used in this example
+    }
+}
