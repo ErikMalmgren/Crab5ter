0363b9efb2d9011c447d39ea5435b296e3313456
Abdulsalam Muhialdeen <slome.lord123@gmail.com>
2023-04-28 11:05:30 +0200

    getWidth

--- app/src/main/java/main/crab5ter/GameView.java
+++ app/src/main/java/main/crab5ter/GameView.java
@@ -102,12 +102,12 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
                 ballY = getHeight() - ballSize;
             }
         }
-        if(ballX-ballSize > getHeight()/4f && ballX-ballSize < (getHeight()/4f)+20){
+        if(ballX-ballSize > getWidth()/4f && ballX-ballSize < (getWidth()/4f)+20){
             ballSpeedX = -ballSpeedX * 0.3f;
-            if(ballX-ballSize < (getHeight()/4f)+20 && ballX-ballSize > (getHeight()/4f)+10){
-                ballX = ballSize + (getHeight()/4f)+20;
-            }else if(ballX+ballSize > getHeight()/4f && ballX+ballSize < (getHeight()/4f)+10){
-                ballX = getHeight()/4f - ballSize;
+            if(ballX-ballSize < (getWidth()/4f)+20 && ballX-ballSize > (getWidth()/4f)+10){
+                ballX = ballSize + (getWidth()/4f)+20;
+            }else if(ballX+ballSize > getWidth()/4f && ballX+ballSize < (getWidth()/4f)+10){
+                ballX = getWidth()/4f - ballSize;
             }
         }
 
