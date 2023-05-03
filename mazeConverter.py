e9514c51784413be44e77d4ae6d05f7094524b45
Erik Malmgren <er8540ma-s@student.lu.se>
2023-05-03 15:27:13 +0200

    mazen igen

--- mazeConverter.py
+++ mazeConverter.py
@@ -1,35 +1,20 @@
-# Använd detta verktyget för att generera input https://www.dcode.fr/maze-generator
+def convertToJavaMatrix(input):
+  rows = input.strip().split('\n')
+  matrix = [[int(cell) for cell in row.strip()] for row in rows]
+  javaArray = [f"{{{','.join(str(cell) for cell in row)}}}" for row in matrix]
+  return "int[][] maze = {" + ','.join(javaArray) + "};"
 
-input_str =  """0001111111
-                0000000001
-                1001111001
-                1000001001
-                1001111111
-                1001001001
-                1001001001
-                1000001001
-                1001001001
-                100100000
-                111111100"""
+input = '''0001111111
+0000000001
+1001111001
+1000001001
+1001111111
+1001001001
+1001001001
+1000001001
+1001001001
+100100000
+111111100'''
 
-# Dela upp strängen i rader
-input_rows = input_str.split("\n")
-
-# Skapa en matris med rätt storlek fylld med nollor
-output_matrix = [[0] * len(input_rows[0]) for _ in range(len(input_rows))]
-
-# Fyll i matrisen baserat på input-strängen
-for i in range(len(input_rows)):
-    for j in range(len(input_rows[0])):
-        if input_rows[i][j] == "1":
-            output_matrix[i][j] = 1
-        elif input_rows[i][j] == "0":
-            output_matrix[i][j] = 0
-        elif input_rows[i][j] == "-":
-            output_matrix[i][j] = -1
-
-# Skriv ut matrisen som kod som kan kopieras in i Java
-print("{")
-for row in output_matrix:
-    print("    {" + ",".join(str(x) for x in row) + "},")
-print("}")
+javaMatrix = convertToJavaMatrix(input)
+print(javaMatrix)
